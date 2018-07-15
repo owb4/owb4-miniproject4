4f1b09ec1777513a8017766a5c4a8cfed526d5c5
Oliver <owb4@njit.edu>
2018-07-15 18:13:21 -0400

    Added user profile, migration, mode, and updated user seeder to create profiles

--- app/User.php
+++ app/User.php
@@ -45,4 +45,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function profile()
+    {
+        return $this->hasOne('App\Profile');
+    }
+
 }
