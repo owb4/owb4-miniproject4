a8e41ebb23da2f29c18ea286cfad6717d962ddc3
Oliver <owb4@njit.edu>
2018-07-29 17:35:39 -0400

    Added answer view and controllers

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,9 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/user/{user_id}/profile', 'ProfileController@create')->name('profile.create');
 Route::get('/user/{user_id}/profile/{profile_id}', 'ProfileController@show')->name('profile.show');
 
+Route::get('/questions/{question_id}/answers/create', 'AnswerController@create')->name('answers.create');
+Route::get('/questions/{question_id}/answers/{answer_id}', 'AnswerController@show')->name('answers.show');
+
 Route::resources([
     'questions' => 'QuestionController',
 ]);
