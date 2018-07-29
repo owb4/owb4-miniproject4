14c30bc6084d4434985ddea3ea49e5937198fbcd
Oliver <owb4@njit.edu>
2018-07-29 17:17:52 -0400

    Added question view and controllers

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/user/{user_id}/profile', 'ProfileController@create')->name('profile.create');
 Route::get('/user/{user_id}/profile/{profile_id}', 'ProfileController@show')->name('profile.show');
+
+Route::resources([
+    'questions' => 'QuestionController',
+]);
