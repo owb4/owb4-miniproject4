339dbdc9bd7c5738e33dc358da511ac5241cb5f5
Oliver <owb4@njit.edu>
2018-07-29 17:03:19 -0400

    Added profile view and controllers

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -63,10 +63,10 @@
 
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                                 @if (Auth::user()->profile)
-                                    <a class="dropdown-item" href="#">My Profile</a>
+                                    <a class="dropdown-item" href="{{ route('profile.show', ['user_id' => Auth::user()->id,'profile_id' => Auth::user()->profile->id]) }}">My Profile</a>
 
                                 @else
-                                    <a class="dropdown-item" href="#">Create Profile</a>
+                                    <a class="dropdown-item" href="{{ route('profile.create', ['user_id' => Auth::user()->id]) }}">Create Profile</a>
                                 @endif
 
 
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/user/{user_id}/profile', 'ProfileController@create')->name('profile.create');
+Route::get('/user/{user_id}/profile/{profile_id}', 'ProfileController@show')->name('profile.show');
