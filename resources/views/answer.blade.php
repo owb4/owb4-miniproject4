d190387b670481fde857f25bd0e361e3b05a6326
Oliver <owb4@njit.edu>
2018-07-30 19:05:04 -0400

    Made back link easier to understand

--- resources/views/answer.blade.php
+++ resources/views/answer.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-12"><a class="float-left" href="{{ route('questions.show',['question_id'=> $question])}}">Back</a></div>
+            <div class="col-md-12"><a class="float-left" href="{{ route('questions.show',['question_id'=> $question])}}">< Back</a></div>
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">Answer</div>
